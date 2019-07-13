09eea4cf9c0b75791da3b52c95b350880ef1102a
raphaelmoline <raphael.moline@gmail.com>
2019-07-13 13:53:23 +0800

    using the default diamonds explorer app

--- myApp/server.R
+++ myApp/server.R
@@ -1,14 +1,30 @@
-library(UsingR)
-data(galton)
+library(shiny)
+library(ggplot2)
 
-shinyServer(  
-    function(input, output) {    
-        output$myHist <- renderPlot({      
-            hist(galton$child, xlab='child height', col='lightblue',main='Histogram')      
-            mu <- input$mu      
-            lines(c(mu, mu), c(0, 200),col="red",lwd=5)      
-            mse <- mean((galton$child - mu)^2)      
-            text(63, 150, paste("mu = ", mu))      
-            text(63, 140, paste("MSE = ", round(mse, 2)))      
-        })      }
-)
+function(input, output) {
+    
+    dataset <- reactive({
+        diamonds[sample(nrow(diamonds), input$sampleSize),]
+    })
+    
+    output$plot <- renderPlot({
+        
+        p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+        
+        if (input$color != 'None')
+            p <- p + aes_string(color=input$color)
+        
+        facets <- paste(input$facet_row, '~', input$facet_col)
+        if (facets != '. ~ .')
+            p <- p + facet_grid(facets)
+        
+        if (input$jitter)
+            p <- p + geom_jitter()
+        if (input$smooth)
+            p <- p + geom_smooth()
+        
+        print(p)
+        
+    }, height=700)
+    
+}
--- myApp/ui.R
+++ myApp/ui.R
@@ -1,8 +1,29 @@
-shinyUI(pageWithSidebar(  
-    headerPanel("Example plot"),  
-    sidebarPanel(    
-        sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)  ), 
-    mainPanel(    
-        plotOutput('myHist')  
+library(shiny)
+library(ggplot2)
+
+dataset <- diamonds
+
+fluidPage(
+    
+    titlePanel("Diamonds Explorer"),
+    
+    sidebarPanel(
+        
+        sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                    value=min(1000, nrow(dataset)), step=500, round=0),
+        
+        selectInput('x', 'X', names(dataset)),
+        selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+        selectInput('color', 'Color', c('None', names(dataset))),
+        
+        checkboxInput('jitter', 'Jitter'),
+        checkboxInput('smooth', 'Smooth'),
+        
+        selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+        selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+    ),
+    
+    mainPanel(
+        plotOutput('plot')
     )
-))
+)
